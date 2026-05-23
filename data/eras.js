242f24318c4eda8226dfbf02e5c93a38eb710329
unknown <zjtjensen@yahoo.com>
2026-05-23 12:03:12 -0400

    time line of war of the ancients, took 7 years

--- data/eras.js
+++ data/eras.js
@@ -47,6 +47,10 @@ const WORLD_EVENTS = [
   reasoning: [
     { description: 'Valahi was founded at the start of the war of the ancients', url: 'https://legendarystories.net/world-of-rathe/aria.html#valahai' }
   ] },
+  { year: -7.5,               label: 'The War of the Ancients begins', 
+    reasoning: [
+      { description: 'Aurora slid the symbols for 90 into place', url: 'https://legendarystories.net/main-story/rosetta/secret-of-the-aetherscribes.html' }
+    ] },
   { year:    0,   type: 'age', label: 'End of Third Age\nFourth Age begins' },
   { year:    0,                label: 'War of the Ancients ends',
     reasoning: [
